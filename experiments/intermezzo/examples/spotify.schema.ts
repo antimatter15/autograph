47eb874f06843045b8ec27eb2b59983e43c9f1ef
Kevin Kwok <antimatter15@gmail.com>
2019-06-14 16:43:06 -0700

    adding spotify demo

---
+++ experiments/intermezzo/examples/spotify.schema.ts
@@ -0,0 +1,43 @@
+type GQLType = {
+    /** The name of the object type */
+    __typename: string
+}
+
+export type Query = GQLType & {
+    hi?(args: { message?: string }): string
+    queryArtists?(args: { byName?: string }): Artist[]
+
+    /** Check this to determine whether the query is loading */
+    _loading?: boolean
+    /** Check this to display error messages */
+    _error?: any
+    /** This field is defined when Autograph is executing a dry run */
+    _dry?: boolean
+}
+
+export type Artist = GQLType & {
+    name: string
+    id?: ID
+    image?: string
+    albums?(args: { limit?: Int }): Album[]
+}
+
+/** The `ID` scalar type represents a unique identifier, often used to refetch an object or as key for a cache. The ID type appears in a JSON response as a String; however, it is not intended to be human-readable. When expected as an input type, any string (such as `"4"`) or integer (such as `4`) input value will be accepted as an ID. */
+export type ID = string
+
+/** The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.  */
+export type Int = number
+
+export type Album = GQLType & {
+    name?: string
+    id?: ID
+    image?: string
+    tracks?: Track[]
+}
+
+export type Track = GQLType & {
+    name: string
+    artists?: Artist[]
+    preview_url?: string
+    id?: ID
+}
---
+++ experiments/intermezzo/examples/spotify.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { createRoot, useQuery, D } from '../autograph'
+import * as GQL from './spotify.schema'
+
+const AutographRoot = createRoot({
+    client: 'https://spotify-graphql-server.herokuapp.com/graphql'
+})
+
+
+function App() {
+    // console.log('render')
+    let [text, setText] = React.useState('Coldplay')
+
+    let query: GQL.Query = useQuery();
+
+    if(query._error) return <div>{query._error.toString()}</div>
+
+    console.log(query)
+    
+    let artists = query.queryArtists({ byName: text }) || [];
+
+    console.log(artists )
+    // console.log(query._dry, query._loading, pokemon)
+    
+    // if(query._loading) return <div>Loading...</div>
+    return <div>
+        <input key="thang" onBlur={e => {
+        }}type="text" value={text} onChange={e => setText(e.target.value)} />
+       {query._loading ? <div>Loading...</div> : <table><tbody>
+           {artists.map(artist => <Artist artist={artist} key={artist.id} />)}
+           </tbody></table>}
+    </div>
+}
+
+function Artist({ artist }: { artist: GQL.Artist }){
+    return <tr >
+    <td style={{verticalAlign: 'top'}}>
+        {artist.image ? <img src={artist.image} style={{ width: 200 }} /> : 'N/A' }</td>
+    <td style={{verticalAlign: 'top'}}><h2>{artist.name}</h2></td>
+<td style={{verticalAlign: 'top'}}>
+    <table>
+        <tbody>
+            {
+                artist.albums({}).map(album => <tr key={album.id}>
+                    <td>
+                    {album.image ? <img src={album.image} style={{ width: 100 }} /> : 'N/A'}
+                    </td>
+                    <td>
+                        {album.name}
+                    </td>
+                </tr>)
+            }
+        </tbody>
+    </table></td>
+   </tr>
+}
+
+// let dataCache = {}
+// let promises = {}
+
+// function App2(){
+//     let [text, setText] = React.useState('Coldplay')
+//     // if(text !== 'Coldplay'){
+
+//     // }
+//     if(!(text in dataCache)){
+//         if(!(text in promises)){
+//             promises[text] = new Promise(resolve => {
+//                 dataCache[text] = 'wumbo'
+//                 // setTimeout(resolve, 0)
+//                 setImmediate(resolve)
+//             })
+//         }
+//         throw promises[text]
+//     }
+    
+//     return <div>
+//         <button onClick={e => setText('asdf')}>thing</button>
+//         <input key="thang" onBlur={e => {
+//         // debugger
+//     }}type="text" value={text} onChange={e => setText(e.target.value)} />
+
+// </div>
+// }
+
+// let dom = <React.Suspense fallback={<div>what is this placeholder</div>}>
+//     <App2 />
+// </React.Suspense>
+
+
+class ErrorBoundary extends React.Component {
+    static getDerivedStateFromError(error) {
+      // Update state so the next render will show the fallback UI.
+      return { hasError: true };
+    }
+
+    componentDidCatch(err){
+        console.log(err)
+    }
+
+    render() {
+      return this.props.children; 
+    }
+  }
+
+
+let dom = <AutographRoot>
+    <ErrorBoundary><App /></ErrorBoundary>
+</AutographRoot>
+
+ReactDOM.render(dom, document.getElementById('root'))
+
+// global.ag = AutographRoot._root;
+
+// ReactDOM.unstable_createRoot(document.getElementById('root')).render(dom)
