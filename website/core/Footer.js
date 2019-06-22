214d97d8f9b50e6e863a058238b36f1fe317f4d8
Kevin Kwok <antimatter15@gmail.com>
2019-06-21 21:35:31 -0400

    minor site changes

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -83,7 +83,7 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
+        {/*<a
           href="https://opensource.facebook.com/"
           target="_blank"
           rel="noreferrer noopener"
@@ -94,7 +94,7 @@ class Footer extends React.Component {
             width="170"
             height="45"
           />
-        </a>
+        </a>*/}
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
