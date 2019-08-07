c67536769fb19b7e63821e6124edb4c342b18b3f
Kevin Kwok <antimatter15@gmail.com>
2019-08-06 19:44:48 -0700

    adding implementation of eager

---
+++ src/util/__tests__/eager.ts
@@ -0,0 +1,185 @@
+import { eager, explore } from "../eager";
+
+test('basic test', () => {
+    let yes_count = 0,
+        no_count = 0;
+    explore(() => {
+        if(eager(true)){
+            yes_count++
+        }else{
+            no_count++
+        }
+    })
+    expect(yes_count).toBe(1)
+    expect(no_count).toBe(1)
+})
+
+
+test('nested explore', () => {
+    let yes_count = 0,
+        no_count = 0;
+    explore(() => {
+        if(eager(false)){
+            explore(() => {
+                if(eager(true)){
+                    yes_count++
+                }else{
+                    no_count++
+                }
+            })
+        }
+    })
+    expect(yes_count).toBe(1)
+    expect(no_count).toBe(1)
+})
+
+
+test('no branch', () => {
+    let execs = 0;
+    explore(() => {
+        execs++;
+    })
+    expect(execs).toBe(1)
+})
+
+
+test('no explore', () => {
+    expect(eager(true)).toBe(true)
+    expect(eager(false)).toBe(false)
+})
+
+test('errors do not affect stack', () => {
+    try {
+            explore(() => {
+            if(eager(false)){
+                throw new Error('blah')
+            }
+        })
+    }catch(e){
+
+    }
+
+    let yes_count = 0,
+        no_count = 0;
+    explore(() => {
+        if(eager(true)){
+            yes_count++
+        }else{
+            no_count++
+        }
+    })
+    expect(yes_count).toBe(1)
+    expect(no_count).toBe(1)
+})
+
+test('else chains', () => {
+
+    let results: Array<string> = []
+    explore(() => {
+        let conds = ''
+        if(eager(true)){
+            conds += 'A'
+        }else if(eager(false)){
+            conds += 'B'
+        }else if(eager(true)){
+            conds += 'C'
+        }else if(eager(true)){
+            conds += 'D'
+        }else{
+            conds += 'E'
+        }
+        results.push(conds)
+    })
+    expect(results).toEqual([ 'A', 'C', 'B', 'D', 'E' ])
+})
+
+
+test('independent branches 3', () => {
+    let results: Array<string> = []
+    explore(() => {
+        let conds = ''
+        if(eager(true)){
+            conds += 'A'
+        }else{
+            conds += 'B'
+        }
+        if(eager(false)){
+            conds += 'C'
+        }else{
+            conds += 'D'
+        }
+        if(eager(true)){
+            conds += 'E'
+        }else{
+            conds += 'F'
+        }
+        results.push(conds)
+    })
+    expect(results).toEqual([ 'ADE', 'BDE', 'ACE', 'ADF', 'BCE', 'BDF', 'ACF', 'BCF' ])
+})
+
+
+test('independent branches 2', () => {
+    let results: Array<string> = []
+    explore(() => {
+        let conds = ''
+        if(eager(true)){
+            conds += 'A'
+        }else{
+            conds += 'B'
+        }
+        if(eager(false)){
+            conds += 'C'
+        }else{
+            conds += 'D'
+        }
+        results.push(conds)
+    })
+    expect(results).toEqual([ 'AD', 'BD', 'AC', 'BC' ])
+})
+
+
+
+test('nested branches 2', () => {
+    let results: Array<string> = []
+    explore(() => {
+        let conds = ''
+        if(eager(true)){
+            if(eager(false)){
+                conds += 'A'
+            }else{
+                conds += 'B'
+            }
+        }else{
+            if(eager(false)){
+                conds += 'C'
+            }else{
+                conds += 'D'
+            }
+        }
+        results.push(conds)
+    })
+    expect(results).toEqual([ 'B', 'D', 'A', 'C' ])
+})
+
+
+
+test('nested branches 1', () => {
+    let results: Array<string> = []
+    explore(() => {
+        let conds = ''
+        if(eager(true)){
+            if(eager(false)){
+                conds += 'A'
+            }else{
+                conds += 'B'
+            }
+        }else{
+            conds += 'C'
+        }
+        results.push(conds)
+    })
+    expect(results).toEqual( [ 'B', 'C', 'A' ])
+})
+
+
---
+++ src/util/eager.ts
@@ -0,0 +1,47 @@
+type State = {
+    index: number
+    queue: Array<Array<boolean>>
+    stack: Array<boolean>
+}
+var state_stack: Array<State> = []
+
+
+export function eager(x: boolean){
+    if(state_stack.length === 0) return x;
+
+    let state = state_stack[state_stack.length - 1];
+    // increment the index pointer
+    state.index++
+    // check if there is a known override that we should be using
+    if(state.stack[state.index] !== undefined){
+        // TODO: detect when there is non-determinism and emit a warning
+        return state.stack[state.index]
+    }else{
+        state.stack[state.index] = x;
+        // create a dual for the current stack where the value is different
+        // and append it to the exploration queue
+        let dual = state.stack.slice(0)
+        dual[state.index] = !x;
+        state.queue.push(dual)
+        return x
+    }
+}
+
+
+export function explore(fn: () => void){
+    let state = {
+        index: 0,
+        queue: [[]],
+        stack: []
+    }
+    state_stack.push(state)
+    try {
+        while(state.queue.length > 0){
+            state.stack = state.queue.shift()!
+            state.index = -1
+            fn()
+        }
+    } finally {
+        state_stack.pop()
+    }
+}
