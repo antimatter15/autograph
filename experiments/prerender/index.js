baec8023f380dd983404946937f78c67f3dc5a8b
Kevin Kwok <antimatter15@gmail.com>
2019-10-10 23:02:07 -0700

    iterating on batching experiments

--- experiments/prerender/index.js
+++ experiments/prerender/index.js
@@ -1,82 +1,128 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as Scheduler from "scheduler";
+import * as Scheduler from 'scheduler'
 
 const Context = React.createContext()
 
 let cache = {}
-function useData(input){
+function useData(input) {
     let ctx = React.useContext(Context)
 
-    if(ctx === 100){
+    if (ctx === 100) {
         return 0
     }
 
-    if(input in cache){
+    if (input in cache) {
         return cache[input]
     }
 
-    throw Promise.resolve(-input).then(async k => {
+    throw Promise.resolve(-input).then(async (k) => {
         let batch = root.createBatch()
-        batch.render(<Context.Provider value={100}>
-            <React.Suspense fallback={<div>Loading...</div>}>
-                <App />
-            </React.Suspense>
-        </Context.Provider>)
-        await batch;
+        batch.render(
+            <Context.Provider value={100}>
+                <React.Suspense fallback={<div>Loading...</div>}>
+                    <App />
+                </React.Suspense>
+            </Context.Provider>
+        )
+        await batch
 
-        cache[input] = k;
-
+        cache[input] = k
 
         // await delay(1000)
         // batch.commit()
         // let batch = root.createBatch()
         // let batch = root.createBatch()
-        batch.render(<Context.Provider value={42}>
-            <React.Suspense fallback={<div>Loading...</div>}>
-                <App />
-            </React.Suspense>
-        </Context.Provider>)
-        await batch;
-        batch.commit()
 
-        
+        // batch = root.createBatch()
+        batch.render(
+            <Context.Provider value={42}>
+                <React.Suspense fallback={<div>Loading...</div>}>
+                    <App />
+                </React.Suspense>
+            </Context.Provider>
+        )
+        await batch
+        batch.commit()
     })
 }
 
-
-function delay(ms){
-    return new Promise(resolve => setTimeout(resolve, ms))
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-function App(){
-    let [ state, setState ] = React.useState(0)
+function App() {
+    let [state, setState] = React.useState(0)
     let ctx = React.useContext(Context)
 
     console.log('called render', state, ctx)
-    let info = useData(state);
+    let info = useData(state)
 
-    return <div>
-        hello world {ctx} {state} {info}
+    return (
+        <div>
+            hello world {ctx} {state} {info}
+            <button
+                onClick={(e) => {
+                    Scheduler.unstable_next(() => {
+                        setState((x) => x + 1)
+                    })
+                }}
+            >
+                increment (defer)
+            </button>
+            <button
+                onClick={(e) => {
+                    // Scheduler.unstable_next(() => {
+                    setState((x) => x + 1)
+                    // })
+                }}
+            >
+                increment (sync)
+            </button>
+            <button
+                onClick={async (e) => {
+                    let batch = root.createBatch()
+                    batch.render(
+                        <Context.Provider value={100}>
+                            <React.Suspense fallback={<div>Loading...</div>}>
+                                <App />
+                            </React.Suspense>
+                        </Context.Provider>
+                    )
+                    await batch
 
-        <button onClick={e => {
-            Scheduler.unstable_next(() => {
-                setState(x => x + 1)
-            });
-        }}>increment</button>
+                    // cache[input] = k
 
-        <button onClick={async e => {
-            
-        }}>do stuff</button>
-    </div>
+                    // await delay(1000)
+                    // batch.commit()
+                    // let batch = root.createBatch()
+                    // let batch = root.createBatch()
+
+                    // batch = root.createBatch()
+                    batch.render(
+                        <Context.Provider value={42}>
+                            <React.Suspense fallback={<div>Loading...</div>}>
+                                <App />
+                            </React.Suspense>
+                        </Context.Provider>
+                    )
+                    await batch
+                    batch.commit()
+                }}
+            >
+                do stuff
+            </button>
+        </div>
+    )
 }
 
 let root = ReactDOM.unstable_createRoot(document.getElementById('root'))
-root.render(<Context.Provider value={42}>
-    <React.Suspense fallback={<div>Loading...</div>}>
-        <App />
-    </React.Suspense>
-</Context.Provider>)
+root.render(
+    <Context.Provider value={42}>
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <App />
+        </React.Suspense>
+    </Context.Provider>
+)
 
-
-global.root = root;
+global.root = root
